4cee09fd2387cebc5f9ac9838c0002814a888d87
GitFraize <zabrovsky2000denis@gmail.com>
2022-06-07 00:53:01 +0300

    Save changes

---
+++ SimplexApp/Models/UserData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplexApp.Models
+{
+    public enum FunctionType
+    {
+        maximum,
+        minimum
+    }
+    public struct UserData
+    {
+        public int countOfLimits;
+        public int countOfVariables;
+        public FunctionType functionType;
+
+        public UserData(int limits, int variables, FunctionType type)
+        {
+            this.countOfLimits = limits;
+            this.countOfVariables = variables;
+            this.functionType = type;
+        }
+    }
+}
---
+++ SimplexApp/UserDataHolder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplexApp
+{
+    internal class UserDataHolder
+    {
+    }
+}
